873cb37ea86d9fa33cd2d16e205a019f4367dcaa
kenellysra <kenellyalmeida@gmail.com>
2019-02-02 23:47:12 +0000

    refact: sql server support

--- src/insert_ceap.py
+++ src/insert_ceap.py
@@ -1,6 +1,6 @@
 import os
 import csv
-import mysql.connector
+import pyodbc
 
 
 FILE_PATH = os.getenv('FILE_PATH')
@@ -8,7 +8,7 @@ MONETARY_COLUMNS = [16, 17, 18, 26]
 BATCH_SIZE = 1000
 
 SQL_INSERT = """
-INSERT INTO ceap_despesas (
+ INSERT INTO ceap_despesas (
     ceap_nome_parlamentar, 
     ceap_cadastro, 
     ceap_carteira_parlamentar, 
@@ -39,46 +39,47 @@ INSERT INTO ceap_despesas (
     ceap_num_deputado,
     ceap_id_documento
     )
-    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
-            %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);            
+    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,
+            ?,?,?,?,?,?,?,?,?,?,?,?,?,?);            
 """
 
 
 def main():
     try:
-        connection = mysql.connector.connect(
-            host=os.getenv('HOST_NAME'),
-            database=os.getenv('DATABASE_NAME'),
-            user=os.getenv('USER_NAME'),
-            password=os.getenv('PASSWORD')
+
+        server = os.getenv('HOST_NAME')
+        database = os.getenv('DATABASE_NAME')
+        username = os.getenv('USER_NAME')
+        password = os.getenv('PASSWORD')
+        driver = '{ODBC Driver 17 for SQL Server}'
+        connection = pyodbc.connect(
+            'Driver=' + driver + ';Server=tcp:' + server +
+            ',1433;Database=' + database + ';Uid=' + username +
+            ';Pwd=' + password
         )
+        cursor = connection.cursor()
 
-        if connection.is_connected():
-            db_info = connection.get_server_info()
-            cursor = connection.cursor(prepared=True)
-            print("Connected on", db_info)
+        with open(FILE_PATH) as f:
+            data = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
+            next(data)  # skip header
+            for c, line in enumerate(data):
+                # Replace ',' to '.' for the values in the monetary columns
+                for column in MONETARY_COLUMNS:
+                    line[column] = line[column].replace(',', '.')
+                for i in range(len(line)):
+                    if not line[i]:
+                        line[i] = None
 
-            with open(FILE_PATH) as f:
-                data = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
-                next(data)  # skip header
-                for c, line in enumerate(data):
-                    # Replace ',' to '.' for the values in the monetary columns
-                    for column in MONETARY_COLUMNS:
-                        line[column] = line[column].replace(',', '.')
-                    for i in range(len(line)):
-                        if not line[i]:
-                            line[i] = None
+                cursor.execute(SQL_INSERT, line)
 
-                    cursor.execute(SQL_INSERT, line)
+                if c % BATCH_SIZE == 0:
+                    connection.commit()
+                    print('Processing message {}'.format(c))
 
-                    if c % BATCH_SIZE == 0:
-                        connection.commit()
-                        print('Processing message {}'.format(c))
-
-                connection.commit()
+            connection.commit()
 
 
-    except mysql.connector as error:
+    except pyodbc.DataError as error:
         print("Fail when inserting the record".format(error))
     except Exception as exp:
         print(exp)
